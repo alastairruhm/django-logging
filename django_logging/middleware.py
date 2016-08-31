5589f43b67f7bc7f84e4c8b8f1a3c5670a56ddc6
Ruhm <ruhm.st@gmail.com>
2016-08-31 14:16:52 +0800

    Make middleware compatible with django 1.10 MIDDLEWARES setting

--- django_logging/middleware.py
+++ django_logging/middleware.py
@@ -1,10 +1,15 @@
 from django.db import connections
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
+
 from . import log
 from . import settings
 from .log_object import LogObject, ErrorLogObject, SqlLogObject
 
 
-class DjangoLoggingMiddleware(object):
+class DjangoLoggingMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         error = ErrorLogObject(request, exception)
         log.error(error)
